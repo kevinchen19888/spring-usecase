3adf6cef3e2ad536795232cad4ff57da42b562fc
kevinchen19888 <kevinchen19888@163.com>
2020-09-22 23:16:32 +0800

    rebase 3 commit
    1 gitTest commit
    
    11 gitTest commit
    
    2 gitTest commit

--- springboot-jackson/src/main/java/com/kevin/jackson/util/CarSerializer.java
+++ springboot-jackson/src/main/java/com/kevin/jackson/util/CarSerializer.java
@@ -28,9 +28,8 @@ public class CarSerializer extends StdSerializer<Car> {
     }
 
     public static void main(String[] args) {
-        System.out.println("first commit");
-        System.out.println("second commit");
-        System.out.println("third commit");
+        System.out.println("1 commit");
+        System.out.println("2 commit");
     }
 }
 
